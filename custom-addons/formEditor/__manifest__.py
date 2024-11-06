3d400dbdcf4a84e63027107b055f57fbef323073
Rastler3D <aleksandr.hvalov@mail.ru>
2024-11-06 06:00:34 +0400

    fix

--- custom-addons/formEditor/__manifest__.py
+++ custom-addons/formEditor/__manifest__.py
@@ -12,8 +12,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/template_views.xml',
-        'views/menu_views.xml',
         'wizard/import_results_wizard_view.xml',
+        'views/menu_views.xml',
     ],
     'installable': True,
     'application': True,
